30263dc7c611a5d5eef8c266d0b9f9cab8aefbfd
yuzhu-noodles <qli@snnu.edu.cn>
2021-04-16 17:01:28 +0800

    四数相加和三数相加

--- foursum.cpp
+++ foursum.cpp
@@ -1,50 +1,111 @@
 #include<iostream>
 using namespace std;
+#include<vector>
+#include<algorithm>
 
+//双指针法
 
-class Person
+//三数相加
+vector <vector<int>>threesum(vector<int>& nums, int target)
 {
-public:
-	void Dispaly()
+	vector<vector<int>>ret;
+	sort(nums.begin(), nums.end());
+	for (int i = 0; i < nums.size(); i++)
 	{
-		cout << "name:" << name << "age:" << age << endl;
+		if (i > 0 && nums[i] == nums[i - 1]) continue;
+		int left = i + 1;
+		int right = nums.size() - 1;
+		while (left < right)
+		{
+			if (nums[i] + nums[left] + nums[right] == target)
+			{
+				ret.push_back(vector<int>{nums[i], nums[left], nums[right]});
+				if (left < right && nums[left] == nums[left++]) left++;
+				if (left < right && nums[right] == nums[right--]) right--;
+			}
+			else if(nums[i] + nums[left] + nums[right] < target)
+			{
+				left++;
+			}
+			else
+			{
+				right--;
+			}
+		}
 	}
-public:
-	char* name;
-	int age;
-};
-
-void test()
-{
-	Person p;
-	p.name = "liqi";
-	p.age = 12;
-	p.Dispaly();
+	return ret;
 }
-//void Person::Dispaly1()
-//{
-//	cout << "name:" << name << "age:" << age << endl;
-//}
+//四数相加
+vector<vector<int>>foursum(vector<int>& nums, int target)
+{
+	vector<vector<int>> ret;
+	sort(nums.begin(), nums.end());
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (i > 0 && nums[i] == nums[i - 1]) continue;
+		for (int j = i+1; j < nums.size(); j++)
+		{
+			if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+			int left = j + 1;
+			int right = nums.size() - 1;
+			while (left < right)
+			{
+				if (nums[i] + nums[j] + nums[left] + nums[right] == target)
+				{
+					ret.push_back(vector<int>{ nums[i] ,nums[j],nums[left],nums[right] });
+					if (left < right && nums[left] == nums[left + 1]) left++;
+					if (left < right && nums[right] == nums[right--]) right--;
+				}
+				else if (nums[i] + nums[j] + nums[left] + nums[right] < target)
+				{
+					left++;
+				}
+				else
+				{
+					right--;
+				}
+			}
+		}
+	}
+	return ret;
+}
+//测试三数相加
+void test_threesum()
+{
+	cout << "三数相加等于0" << endl;
+	int a[6] = { 1,0,-1,0,-2,2 };
+	vector<int> nums(a, a + 6);
+	int target = 0;
+	vector<vector<int>>ret = threesum(nums, target);
+	for (int i = 0; i < ret.size(); i++)
+	{
+		for (int j = 0; j < ret[i].size(); j++)
+		{
+			cout << ret[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+//测试四数相加
+void test_foursum()
+{
+	cout << "四数相加等于0" << endl;
+	int a[6] = { 1,0,-1,0,-2,2 };
+	vector<int> nums(a, a + 6);
+	int target = 0;
+	vector<vector<int>>ret = foursum(nums, target);
+	for (int i = 0; i < ret.size(); i++)
+	{
+		for (int j = 0; j < ret[i].size(); j++)
+		{
+			cout << ret[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 int main()
 {
-	test();
-	//int a = 0;
-	//int& b = a;
-	//cout << a<<"address:" << &a << endl;
-	//cout << b<<"address:" << &b << endl;
-
-	//int& c = b;
-	//cout << "b,address:" << &c << endl;
-
-	//int a = 1;
-	//const int& b = a;
-	//
-	//cout << a << endl;
-	//cout << b << endl;
-	//const int a = 1;
-	//const int& b = a;
-	//const int& c = b;
-	//cout << c;
-
+	test_threesum();
+	test_foursum();
 	return 0;
 }
